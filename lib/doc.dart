43d618587962c6daf6f840c19421a876a748f02b
GregoireSauvage <78805529+GregoireSauvage@users.noreply.github.com>
2024-01-14 20:41:11 +0100

    Amélioration de la portabilité de l'UI (web & mobile) + fonctionnalité ajouter un doc (pdf & img) et visualisation (version web uniquement)

--- lib/doc.dart
+++ lib/doc.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:docare/main.dart';
+import 'package:file_picker/file_picker.dart'; // Pour sélectionner un fichier
+import 'dart:typed_data'; // Pour convertir un fichier en bytes
+import 'dart:html' as html; // Pour afficher une image dans un dialogue
+
+
+import 'package:flutter/foundation.dart'; 
+import 'dart:ui_web' as ui; // Pour afficher un pdf dans un dialogue
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 class Document extends StatelessWidget {
+  
+  // Méthode pour afficher un pdf dans un dialogue
+  Future<void> _displayPdf(BuildContext context, Uint8List fileBytes) async {
+  // Create a Blob from the Uint8List
+  final blob = html.Blob([fileBytes], 'application/pdf');
+  // Create an object URL for the Blob
+  final url = html.Url.createObjectUrlFromBlob(blob);
+
+  // Define a unique ID for the view
+  final uniqueId = 'pdf-viewer-${DateTime.now().millisecondsSinceEpoch}';
+
+  // Register the view factory if not already registered
+  // Note: The new API does not require checking if it's registered
+  ui.platformViewRegistry.registerViewFactory(
+    uniqueId,
+    (int viewId) => html.IFrameElement()
+      ..src = url
+      ..style.border = 'none'
+      ..style.width = '100%'
+      ..style.height = '100%'
+  );
+
+  // Display the PDF in an AlertDialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: Container(
+          height: 400, // Set the height of the dialog
+          width: 600,
+          child: HtmlElementView(viewType: uniqueId),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Fermer'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              html.Url.revokeObjectUrl(url);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
   // Méthode pour construire la barre de recherche
   Widget buildTopBar(BuildContext context) {
@@ -13,25 +66,84 @@ class Document extends StatelessWidget {
     double padding = screenWidth > 600 ? 50.0 : 8.0;
 
     return Container(
+      alignment: Alignment.center,
       padding: EdgeInsets.all(padding),
       color: Colors.blue,
       child: Row(
         children: <Widget>[
-          // Utilisez Flexible pour que le bouton "Ajouter un document" s'adapte à la taille de l'écran
           Flexible(
-            child: ElevatedButton(
-              onPressed: () {
-                // Code pour ajouter un document
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.blue,
-                textStyle: TextStyle(
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: buttonWidth),
+              child: ElevatedButton(
+                onPressed: () async {
+                  // async pour pouvoir utiliser await
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(
+                    // Sélectionner un fichier
+                    type: FileType.custom,
+                    allowedExtensions: [
+                      'jpg',
+                      'jpeg',
+                      'png',
+                      'pdf'
+                    ], // Extensions de fichier autorisées
+                  );
+
+                  if (result != null) {
+                    PlatformFile file = result.files.first;
+
+                    // Vérifiez si le fichier est une image
+                    if (['jpg', 'jpeg', 'png'].contains(file.extension)) {
+                      // Convertir en Uint8List
+                      Uint8List fileBytes = file.bytes!;
+                      // Créer une image à partir des bytes
+                      Widget image = Image.memory(fileBytes);
+
+                      // Afficher l'image dans un dialogue
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: image,
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Fermer'),
+                              onPressed: () => Navigator.of(context).pop(),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else if (file.extension == 'pdf') {
+                      PlatformFile file = result.files.first; // Récupérer le fichier
+                      Uint8List? fileBytes = file.bytes; // Convertir en Uint8List
+                      if (fileBytes != null) {
+                        await _displayPdf(context, fileBytes); // Appelle la methode pour afficher le pdf
+                      }
+
+                    } else {
+                      // Gérer les autres types de fichiers ici
+                      print(
+                          'Type de fichier non supporté pour la visualisation directe.');
+                    }
+                  } else {
+                    // L'utilisateur a annulé la sélection de fichier
+                    print('Aucun fichier sélectionné.');
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.blue,
+                  textStyle: TextStyle(
                     fontSize: screenWidth > 600
                         ? 20
-                        : 14), // Ajustez la taille de la police en fonction de la largeur de l'écran
+                        : 14, // Ajuster cette valeur si nécessaire
+                  ),
+                ),
+                child: screenWidth > 700
+                    ? Text(
+                        'Ajouter un document') // Si l'écran est large (texte)
+                    : Icon(Icons.add,
+                        color: Colors.blue), // Si l'écran est petit (icone)
               ),
-              child: const Text('Ajouter un document'),
             ),
           ),
           const SizedBox(width: 8.0),
@@ -55,19 +167,24 @@ class Document extends StatelessWidget {
           const SizedBox(width: 8.0),
           // Utilisez Flexible pour que le bouton "Rechercher" s'adapte à la taille de l'écran
           Flexible(
-            child: ElevatedButton(
-              onPressed: () {
-                // Code pour la recherche
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.blue,
-                textStyle: TextStyle(
-                    fontSize: screenWidth > 600
-                        ? 20
-                        : 14), // Ajuster la taille de la police en fonction de la largeur de l'écran
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: buttonWidth),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Code pour ajouter un document
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.blue,
+                  textStyle: TextStyle(
+                    fontSize: screenWidth > 600 ? 20 : 14,
+                  ),
+                ),
+                child: screenWidth > 700
+                    ? Text('Rechercher') // Si l'écran est large (texte)
+                    : Icon(Icons.search,
+                        color: Colors.blue), // Si l'écran est petit (icone)
               ),
-              child: const Text('Rechercher'),
             ),
           ),
         ],
@@ -117,7 +234,8 @@ class Document extends StatelessWidget {
               ],
             ),
           ),
-          buildTopBar(context), // Barre de recherche (voir la méthode buildTopBar)
+          buildTopBar(
+              context), // Barre de recherche (voir la méthode buildTopBar)
           Expanded(
             child: GridView.builder(
               padding: const EdgeInsets.all(4.0),
---
+++ lib/fake_ui.dart
@@ -0,0 +1,7 @@
+// A placeholder for non-web platforms.
+
+class PlatformViewRegistry {
+  static void registerViewFactory(String viewId, dynamic cb) {
+    // This is just a stub implementation for non-web platforms.
+  }
+}
